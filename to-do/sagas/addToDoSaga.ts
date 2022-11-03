214eb25b97766353b7dc941c6325d9d25c116b4a
harry-vcnc <harry@vcnc.co.kr>
2022-11-03 11:33:17 +0900

    메모 제거

--- to-do/sagas/addToDoSaga.ts
+++ to-do/sagas/addToDoSaga.ts
@@ -3,13 +3,6 @@ import { fork, put, take, takeLeading } from 'typed-redux-saga';
 import { fetchAddToDo } from './apis';
 
 export function* addToDoSaga(action: RequestAddToDoActionType) {
-  // 큰 타입 action으로 넘겨주면, 이 사가에서 호출되는
-  // 제너레이터 혹은 사가 내부에 사용한다
-
-  // 사가 합성도 다음처럼 가능함
-  // yield* fork('saga', 'action');
-  // yield* call('saga', 'action');
-
   yield* fork(fetchAddToDo, action);
   const result = yield* take([
     todoActions.successAddToDoApi.type,
@@ -18,7 +11,6 @@ export function* addToDoSaga(action: RequestAddToDoActionType) {
 
   if (result.type === todoActions.failureAddToDoApi.type) {
     yield* put(todoActions.failureAddToDo());
-    // 제너레이터에서 리턴은?
     return;
   }
 
--- to-do/sagas/deleteToDoSaga.ts
+++ to-do/sagas/deleteToDoSaga.ts
@@ -47,20 +47,6 @@ function* retryDeleteToDoSaga(action: RequestDeleteToDoActionType) {
   yield* put(todoActions.successRetryDeleteToDo());
 }
 
-// api watcher?
-// 어떤 흐름을 통제하느냐에 따라 takeEvery latest등이 달라질 수 있다
-// 그러므로 api는 그대로 두고 이것을 호출하는 사가에서 통제하는 것으로 하는게 좋음
-// 즉 api watcher는 필요 없음
-
-// 진입점과 끝나는 지점이 단 하나씩
-// 진입점은 UI로 생각하면 되고 끝내는 지점은 사가에서 끝낼 것이다
-// 즉 지금처럼 사가에서 본인을 부르면 진입점이 여러 개인 형태
-
-// 근데 유저가 자의적으로 캔슬하는 경우 failure로 두어야 하나?
-// 그냥 success failure boolean하게 시작과 끝으로 두고, 그 안에 children으로 들어가는 형식으로
-// 왜냐? 그것은 UI적인 사고 -> saga에서는 '실행 흐름'의 성공 or 실패로 사고해야
-// 사실 그런 식으로 작성하다보면 confirm, deny, decline 등등등 유저 동작에 따라 액션이 너무 많아짐
-
 export const deleteToDoWatcher = [
   takeLeading(todoActions.requestDeleteToDo.type, deleteToDoSaga),
   takeLatest(todoActions.requestRetryDeleteToDo.type, retryDeleteToDoSaga),
